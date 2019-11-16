540d24ccbe7e921b9bafb40a54d0be2ad725274c
David Somers-Harris <david@somers-harris.com>
2019-11-16 17:36:30 +0800

    more verbosity

--- forwarder.py
+++ forwarder.py
@@ -1,14 +1,18 @@
 from socket import socket, AF_PACKET, SOCK_RAW
 from netifaces import interfaces, ifaddresses
 
+print("Printing Interfaces...")
 for interface in interfaces():
     print(interface)
     print(ifaddresses(interface))
     print()
 
+print("Binding to socket...")
 s = socket(AF_PACKET, SOCK_RAW)
 s.bind(("eth0", 0))
 
+print("s.recv(4096...")
 obj = s.recv(4096)
 
+print("Printing object...")
 print(obj)
