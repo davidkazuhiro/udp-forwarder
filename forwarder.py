87b14f771b5551727215a9602a96dab808e1bfe9
David Somers-Harris <david@somers-harris.com>
2019-11-17 11:37:41 +0800

    code cleanup

--- forwarder.py
+++ forwarder.py
@@ -1,21 +1,29 @@
 from netifaces import interfaces, ifaddresses
 from select import select
-from socket import socket, AF_PACKET, SOCK_RAW, getprotobyname, ntohs
+from socket import socket, AF_PACKET, SOCK_RAW, ntohs, htons
+from struct import unpack
 
-interface = "eth0"
-protocol = getprotobyname('udp')
+def ethernet_frame(data):
+    dest_mac, src_mac, proto = unpack('! 6s 6s H', data[:14])
+    return get_mac_addr(dest_mac), get_mac_addr(src_mac), htons(proto), data[14:]
 
-print("\nPrinting Interfaces...")
+def get_mac_addr(bytes_addr):
+    bytes_str = map('{:02x}'.format, bytes_addr)
+    return ':'.join(bytes_str).upper()
+
 for interface in interfaces():
     print(interface)
     for entry in ifaddresses(interface):
         print(ifaddresses(interface)[entry][0]['addr'])
     print()
 
-print("Binding to socket...")
 s = socket(AF_PACKET, SOCK_RAW, ntohs(3))
 
 print("Receiving packets...")
 while True:
     raw_data, addr = s.recvfrom(65535)
-    print(raw_data, addr)
+    dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
+    print(f"Destination: {dest_mac}")
+    print(f"Source: {src_mac}")
+    print(f"Protocol: {eth_proto}")
+    print(f"Data: {data}")
